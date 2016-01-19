4f8bc92d10b733033d95e6e64f4f2ec81f9b1f2d
joaoibarra <joao.ibarra@gmail.com>
2016-01-19 20:58:49 -0300

    api Yahoo Weather

--- app/src/main/java/br/com/ibarra/clima/api/services/YahooWeatherServiceImpl.java
+++ app/src/main/java/br/com/ibarra/clima/api/services/YahooWeatherServiceImpl.java
@@ -1,7 +1,40 @@
 package br.com.ibarra.clima.api.services;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import br.com.ibarra.clima.api.services.interfaces.YahooWeatherService;
+import okhttp3.OkHttpClient;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Retrofit;
+
 /**
  * Created by joaoibarra on 19/01/16.
  */
 public class YahooWeatherServiceImpl {
+    public static YahooWeatherService yahooWeatherService;
+
+    public static YahooWeatherService getInstance(){
+        if(yahooWeatherService ==null)
+            return new YahooWeatherServiceImpl().create();
+
+        return yahooWeatherService;
+    }
+
+    private YahooWeatherServiceImpl(){}
+
+    public YahooWeatherService create() {
+        final OkHttpClient okHttpClient = new OkHttpClient();
+        Gson gson = new GsonBuilder()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://query.yahooapis.com")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        yahooWeatherService = retrofit.create(YahooWeatherService.class);
+        return yahooWeatherService;
+    }
 }
--- app/src/main/java/br/com/ibarra/clima/api/services/interfaces/YahooWeatherService.java
+++ app/src/main/java/br/com/ibarra/clima/api/services/interfaces/YahooWeatherService.java
@@ -1,7 +1,17 @@
 package br.com.ibarra.clima.api.services.interfaces;
 
+import br.com.ibarra.clima.api.models.CurrentCondition;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
 /**
  * Created by joaoibarra on 19/01/16.
  */
-public class YahooWeatherService {
+public interface YahooWeatherService {
+    //https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in (select woeid from geo.places(1) where text='chicago, il') and u='c'&format=json&callback=callbackFunction
+    @Headers({"Accept: Application/JSON"})
+    @GET("/v1/public/yql")
+    Call<CurrentCondition> getCurrentCondition(@Query("q") String query, @Query("format") String format);
 }
